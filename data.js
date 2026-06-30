345f1964624a0bd27586098637dad1b667bbc5c6
Elena <mrmohmd5@gmail.com>
2026-06-30 11:19:16 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+const globalMoviesData = [
+    {
+        title: "The Super Mario Bros Movie",
+        desc: "في إطار من المغامرات والخيال، يسافر سباك يدعى ماريو عبر متاهة تحت الأرض مع شقيقه لويجي، لإنقاذ أميرة مسجونة.",
+        poster: "https://a.top4top.io/p_3832nihab0.jpg",
+        hdUrl: "https://playmogo.com/e/gjeby3orhit6", // 👈 الرابط شغال وجاهز
+        sdUrl: "https://playmogo.com/e/gjeby3orhit6", 
+        rating: "7/10",
+        genre: "أكشن مغامرة خيالي",
+        type: "movie"
+    },
+    {
+        title: "فيلم جديد لم يرفعه بعد",
+        desc: "هنا تكتب قصة الفيلم الجديد وتفاصيله بشكل عادي جداً...",
+        poster: "https://via.placeholder.com/400x600.png?text=No+Poster", 
+        hdUrl: "", // 👈 اتركه فارغاً هكذا! الموقع سيعرف أنك لم ترفعه بعد وسيخفيه تلقائياً عن الزوار حتى تضع الرابط مستقبلاً
+        sdUrl: "",
+        rating: "8.5",
+        genre: "دراما",
+        type: "movie"
+    }
+];
