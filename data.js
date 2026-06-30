03f2422e498409c32400a6eb377be6c202352d60
Elena <mrmohmd5@gmail.com>
2026-06-30 12:49:07 +0300

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const globalMoviesData = [
         title: "Elemental",
         desc: "في مدينة صاخبة يعيش فيها سكان النار، الماء، الأرض، والنوايا بانسجام، تلتقي إمبير (الفتاة النارية الحادة والذكية) بـ وايد (الشاب المائي المرح والرقاق). هذا اللقاء غير المتوقع يتحدى معتقداتهما عن العالم ويقودهما في رحلة لاكتشاف القواسم المشتركة بينهما، في إطار بصري مذهل وقصة تلمس القلوب وتثبت أن الأضداد تتجاذب فعلاً ",
         poster: "https://images.themoviedb.org/t/p/original/4YDCw9789Y68g668g2as6FFgA1u.jpg", 
-        hdUrl: "https://playmogo.com/e/v4mp3htzt0em", // 
+        hdUrl: "https://playmogo.com/e/v4mp3htzt0em", 
         sdUrl: "https://playmogo.com/e/v4mp3htzt0em",
         rating: "7.0",
         genre: "كوميدي عائلي خيالي ",
@@ -13,7 +13,7 @@ const globalMoviesData = [
         title: "The Super Mario Bros Movie",
         desc: "في إطار من المغامرات والخيال، يسافر سباك يدعى ماريو عبر متاهة تحت الأرض مع شقيقه لويجي، لإنقاذ أميرة مسجونة.",
         poster: "https://a.top4top.io/p_3832nihab0.jpg",
-        hdUrl: "https://playmogo.com/e/gjeby3orhit6", // 👈 الرابط شغال وجاهز
+        hdUrl: "https://playmogo.com/e/gjeby3orhit6", 
         sdUrl: "https://playmogo.com/e/gjeby3orhit6", 
         rating: "7/10",
         genre: "أكشن مغامرة خيالي",
