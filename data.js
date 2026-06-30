21a477f12a9be6f723520d493bd72245a2450b6d
Elena <mrmohmd5@gmail.com>
2026-06-30 13:22:25 +0300

    Refactor globalMoviesData array structure

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const globalMoviesData = [  
+const globalMoviesData = [
     {
         title: "Elemental",
         desc: "في مدينة صاخبة يعيش فيها سكان النار، الماء، الأرض، والنوايا بأن سجام، تلتقي إمبير (الفتاة النارية الحادة والذكية) بـ وايد (الشاب المائي المرح والرقاق). هذا اللقاء غير المتوقع يتحدى معتقداتهما عن العالم ويقودهما في رحلة لاكتشاف القواسم المشتركة بينهما، في إطار بصري مذهل وقصة تلمس القلوب وتثبت أن الأضداد تتجاذب فعلاً.",
@@ -9,14 +9,14 @@ const globalMoviesData = [
         genre: "كوميدي عائلي خيالي",
         type: "movie"
     },
-
     {
         title: "The Super Mario Bros Movie",
         desc: "في إطار من المغامرات والخيال، يسافر سباك يدعى ماريو عبر متاهة تحت الأرض مع شقيقه لويجي، لإنقاذ أميرة مسجونة.",
         poster: "https://a.top4top.io/p_3832nihab0.jpg",
         hdUrl: "https://playmogo.com/e/gjeby3orhit6", 
-        sdUrl: "https://playmogo.com/e/gjeby3orhit6", 
+        sdUrl: "https://playmogo.com/e/gjeby3orhit6",
         rating: "7/10",
         genre: "أكشن مغامرة خيالي",
         type: "movie"
-    },
+    }
+];
