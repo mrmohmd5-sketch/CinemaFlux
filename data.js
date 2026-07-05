04dd4460e86804134fd95dde2841a273ad4bdd72
Elena <mrmohmd5@gmail.com>
2026-07-05 11:49:46 +0300

    إضافة تلقائية آمنة للفيلم: Ant-Man and the Wasp مع طاقم التمثيل الصحيح

--- data.js
+++ data.js
@@ -32,6 +32,38 @@ const globalMoviesData = [
     ]
 },
     {
+    "title": "Ant-Man and the Wasp",
+    "desc": "بينما يوازن سكوت لانغ بين كونه بطلاً خارقاً وأباً، تُقدّم هوب فان داين والدكتور هانك بيم مهمةً جديدةً عاجلةً يجد فيها الرجل النملة نفسه يقاتل جنباً إلى جنب مع الدبور لكشف أسرار من ماضيهما.",
+    "poster": "https://image.tmdb.org/t/p/w500/yFcXy5i8tFU8kxia69WaLL93k0Z.jpg",
+    "hdUrl": "https://playmogo.com/e/vvcg77j2sk80",
+    "sdUrl": "https://playmogo.com/e/vvcg77j2sk80",
+    "rating": "6.9",
+    "genre": "حركة مغامرة خيال علمي",
+    "type": "movie",
+    "cast": [
+        {
+            "name": "Paul Rudd",
+            "image": "https://image.tmdb.org/t/p/w185/6jtwNOLKy0LdsRAKwZqgYMAfd5n.jpg"
+        },
+        {
+            "name": "Evangeline Lilly",
+            "image": "https://image.tmdb.org/t/p/w185/zgztLIWTJZm3vjGU7ezhF8GXESJ.jpg"
+        },
+        {
+            "name": "Michael Douglas",
+            "image": "https://image.tmdb.org/t/p/w185/kVYGPIZowzXLEQfAGUNOqKjAbBb.jpg"
+        },
+        {
+            "name": "Hannah John-Kamen",
+            "image": "https://image.tmdb.org/t/p/w185/V8P9EVelewGVXbuyEylqrzF6iG.jpg"
+        },
+        {
+            "name": "Randall Park",
+            "image": "https://image.tmdb.org/t/p/w185/u1QpbjVIKegk79DJG9HFmg9Ljv4.jpg"
+        }
+    ]
+},
+    {
         "title": "Fast X",
         "desc": "تدور احداث الفيلم حيث ينطلق (دانتي) - ابن زعيم الجريمة (هيرنان رييس) - الذي قُتل قبل عشر سنوات للانتقام من البطل (دوم توريتو) وعائلته وشركائه بعد التسبب في مقتل والده وسرقة أمواله.",
         "poster": "https://image.tmdb.org/t/p/w500/mgBcSHJt8fcRfXn8LcWusrCt0Kw.jpg",
