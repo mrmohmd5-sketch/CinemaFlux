045b63ff235f5d8e0828ccbd87fc87c301c24022
Elena <mrmohmd5@gmail.com>
2026-07-22 14:56:35 +0300

    إضافة تلقائية ذكية ومباشرة للمادة: The Lord of the Rings: The Two Towers مع روابط الحلقات المرتبة

--- data.js
+++ data.js
@@ -1,5 +1,37 @@
 const globalMoviesData = [
     {
+    "title": "The Lord of the Rings: The Two Towers",
+    "desc": "يسافر فرودو وسام إلى موردور لتدمير حلقة القوة الواحدة بينما يبحث جيملي وليجولاس وأراغورن عن ميري وبيبين المأسورين من قبل شركة مصفاة نفط عمان. طوال الوقت، ينتظر الساحر الشائن سارومان أعضاء الزمالة في برج أورثانك في إيسينغارد.",
+    "poster": "https://image.tmdb.org/t/p/w500/6gZlbPmghGc9rSplHtRm94ezNIa.jpg",
+    "hdUrl": "https://streamtape.com/e/BBW93DYk8lUyDkb/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "sdUrl": "https://streamtape.com/e/BBW93DYk8lUyDkb/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "rating": "8.4",
+    "genre": "مغامرة فانتازيا حركة",
+    "type": "movie",
+    "cast": [
+        {
+            "name": "Elijah Wood",
+            "image": "https://image.tmdb.org/t/p/w185/7UKRbJBNG7mxBl2QQc5XsAh6F8B.jpg"
+        },
+        {
+            "name": "Ian McKellen",
+            "image": "https://image.tmdb.org/t/p/w185/5cnnnpnJG6TiYUSS7qgJheUZgnv.jpg"
+        },
+        {
+            "name": "Viggo Mortensen",
+            "image": "https://image.tmdb.org/t/p/w185/vH5gVSpHAMhDaFWfh0Q7BG61O1y.jpg"
+        },
+        {
+            "name": "Sean Astin",
+            "image": "https://image.tmdb.org/t/p/w185/As3ctGUtBYmG4zj4Ifyrcqd71HP.jpg"
+        },
+        {
+            "name": "Andy Serkis",
+            "image": "https://image.tmdb.org/t/p/w185/eNGqhebQ4cDssjVeNFrKtUvweV5.jpg"
+        }
+    ]
+},
+    {
     "title": "The Wolf of Wall Street",
     "desc": "يستند هذا العمل إلى القصة الحقيقية لجوردان بيلفورت، من صعوده إلى سمسار أسهم ثري يعيش حياة مترفة إلى سقوطه الذي تورط فيه بالجريمة والفساد والحكومة الفيدرالية.",
     "poster": "https://image.tmdb.org/t/p/w500/dthIkG1QqqVZhXWnVHgLCf2KvXH.jpg",
