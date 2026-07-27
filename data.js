cd52f5e3145fdba0e314aa9c0772e649c81198bb
Elena <mrmohmd5@gmail.com>
2026-07-27 16:40:07 +0300

    إضافة تلقائية ذكية ومباشرة للمادة: Hotel Transylvania مع روابط الحلقات المرتبة

--- data.js
+++ data.js
@@ -1,5 +1,38 @@
 const globalMoviesData = [
     {
+    "title": "Hotel Transylvania",
+    "year": "2012",
+    "desc": "يمتلك (دراكولا) منتجعًا راقيًا بعيدًا عن أعين البشر، وهو ما يسمح له بتنشئة ابنته في مناخ آمن، وقدرة رفقائه من الوحوش على الاستجمام مع عائلاتهم في منتجعه، فماذا يفعل عندما يكتشف أن هناك صبي يُدعى (جوناثان) يستكشف منتجعه، ويقع في غرام ابنته المراهقة؟",
+    "poster": "https://image.tmdb.org/t/p/w500/laJ53fk8X28zZKlCMpxzJqMu577.jpg",
+    "hdUrl": "https://streamtape.com/e/1bAerAaleeuePj9/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "sdUrl": "https://streamtape.com/e/1bAerAaleeuePj9/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "rating": "7.0",
+    "genre": "رسوم متحركة كوميديا عائلي فانتازيا",
+    "type": "movie",
+    "cast": [
+        {
+            "name": "Selena Gomez",
+            "image": "https://image.tmdb.org/t/p/w185/wAeEHiyCkiW1SME9CCfkCtjb6ni.jpg"
+        },
+        {
+            "name": "Adam Sandler",
+            "image": "https://image.tmdb.org/t/p/w185/iTMnXrPfC1rmom6a9q4hy6YSJWG.jpg"
+        },
+        {
+            "name": "Andy Samberg",
+            "image": "https://image.tmdb.org/t/p/w185/jMXU5oG3i93SH1yhkpbBGskFiJl.jpg"
+        },
+        {
+            "name": "Kevin James",
+            "image": "https://image.tmdb.org/t/p/w185/3WPW5duZyQcjveefxwLULgIyhM0.jpg"
+        },
+        {
+            "name": "Fran Drescher",
+            "image": "https://image.tmdb.org/t/p/w185/6IQlygYDyxuIRD5j9lPluknYoJp.jpg"
+        }
+    ]
+},
+    {
     "title": "Hotel Transylvania 3: Summer Vacation",
     "year": "2018",
     "desc": "دراكولا ومافيس وجوني وبقية أفراد مجموعة دراكولا يقضون إجازة على متن سفينة سياحية فاخرة، حيث يقع دراكولا في حب قائدة السفينة، إريكا، التي تنحدر سراً من سلالة أبراهام فان هيلسينج، قاتل الوحوش سيئ السمعة.",
