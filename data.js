876480f9bdf76924dcc4e2c384db97d11bf864f6
Elena <mrmohmd5@gmail.com>
2026-08-02 00:29:50 +0300

    Update movie title to Arabic translation

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const globalMoviesData = [
     {
-    "title": "Reminder",
+    "title": "تذكر الحب aski hatirla",
     "year": "2025",
     "desc": "يلتقي محترفان مفطورا القلب - مهندس معماري ومحرر - صدفةً بعد انتهاء علاقتهما. تجمعهما رسالة هاتفية غامضة، فتبدأ رحلة رومانسية غير متوقعة وشفاء.",
     "poster": "https://image.tmdb.org/t/p/w500/aID893ooBbZ4fxy2rQvb7MYIlv8.jpg",
