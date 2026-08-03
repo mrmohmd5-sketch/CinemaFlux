3daa6351d08ee4978cf53e33b5bd5cb21a8d9a2f
Elena <mrmohmd5@gmail.com>
2026-08-03 13:46:24 +0300

    إضافة تلقائية ذكية ومباشرة للمادة: Moana 2 مع روابط الحلقات المرتبة

--- data.js
+++ data.js
@@ -1,5 +1,43 @@
 const globalMoviesData = [
     {
+    "title": "Moana 2",
+    "year": "2024",
+    "desc": "بعد تلقيها نداء من أسلافها، تسافر 'موانا' و'ماوي' إلى بحار 'أوقيانوسيا' البعيدة.",
+    "poster": "https://image.tmdb.org/t/p/w500/yhyYzzJOZzdd1pQPDLV6jYulAmC.jpg",
+    "hdUrl": "https://streamtape.com/e/j6L6W71P13izyvd/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "sdUrl": "https://streamtape.com/e/j6L6W71P13izyvd/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "rating": "7.0",
+    "genre": "مغامرة رسوم متحركة كوميديا عائلي فانتازيا",
+    "type": "movie",
+    "cast": [
+        {
+            "name": "Auliʻi Cravalho",
+            "character": "Moana (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/vEroqcnM2g6yY7qXDAie7hx2Cyp.jpg"
+        },
+        {
+            "name": "Dwayne Johnson",
+            "character": "Maui (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/5QApZVV8FUFlVxQpIK3Ew6cqotq.jpg"
+        },
+        {
+            "name": "Hualālai Chung",
+            "character": "Moni (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/x2g5fdHqETY9dZgL4aB0QDP0boR.jpg"
+        },
+        {
+            "name": "Rose Matafeo",
+            "character": "Loto (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/zQa39fMjbOTIsovbh1TBTJVlToz.jpg"
+        },
+        {
+            "name": "David Fane",
+            "character": "Kele (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/tcozyaTgAa8rRmzc5qeht0loni6.jpg"
+        }
+    ]
+},
+    {
     "title": "Into the Wild",
     "year": "2007",
     "desc": "بعد التخرج من جامعة إيموري في عام 1992 ، تخلى الطالب والرياضي كريستوفر ماكاندلس عن ممتلكاته ، وقدم كامل حساب التوفير البالغ 24000 دولار للجمعيات الخيرية ، وركوب الخيل في ألاسكا للعيش في البرية.",
