ef2aaedcf3c41acd864b3bdbdaad8d0a961140b8
Elena <mrmohmd5@gmail.com>
2026-08-03 14:20:27 +0300

    إضافة تلقائية ذكية ومباشرة للمادة: Moana 2 مع روابط الحلقات المرتبة

--- data.js
+++ data.js
@@ -1,5 +1,68 @@
 const globalMoviesData = [
     {
+    "title": "Moana 2",
+    "year": "2024",
+    "desc": "بعد تلقيها نداء من أسلافها، تسافر 'موانا' و'ماوي' إلى بحار 'أوقيانوسيا' البعيدة.",
+    "poster": "https://image.tmdb.org/t/p/w500/yhyYzzJOZzdd1pQPDLV6jYulAmC.jpg",
+    "hdUrl": "https://streamtape.com/e/j6L6W71P13izyvd/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "sdUrl": "https://streamtape.com/e/j6L6W71P13izyvd/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "rating": "7.0",
+    "genre": "مغامرة رسوم متحركة كوميديا عائلي فانتازيا",
+    "type": "movie",
+    "cast": [
+        {
+            "name": "Auliʻi Cravalho",
+            "character": "Moana (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/vEroqcnM2g6yY7qXDAie7hx2Cyp.jpg"
+        },
+        {
+            "name": "Dwayne Johnson",
+            "character": "Maui (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/5QApZVV8FUFlVxQpIK3Ew6cqotq.jpg"
+        },
+        {
+            "name": "Hualālai Chung",
+            "character": "Moni (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/x2g5fdHqETY9dZgL4aB0QDP0boR.jpg"
+        },
+        {
+            "name": "Rose Matafeo",
+            "character": "Loto (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/zQa39fMjbOTIsovbh1TBTJVlToz.jpg"
+        },
+        {
+            "name": "David Fane",
+            "character": "Kele (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/tcozyaTgAa8rRmzc5qeht0loni6.jpg"
+        },
+        {
+            "name": "Awhimai Fraser",
+            "character": "Matangi (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/276OUDPl2iIsz772HQw3tiz2JN2.jpg"
+        },
+        {
+            "name": "Khaleesi Lambert-Tsuda",
+            "character": "Simea (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/3LHXDjy9UijbtR7X2EReX5H57kk.jpg"
+        },
+        {
+            "name": "Temuera Morrison",
+            "character": "Chief Tui (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/AvtSC0f9QW7fMyFFNXEWDeQyfUk.jpg"
+        },
+        {
+            "name": "Nicole Scherzinger",
+            "character": "Sina (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/kKG7zJvVReTipBYDD3aqHj4uPBe.jpg"
+        },
+        {
+            "name": "Rachel House",
+            "character": "Gramma Tala (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/2LCQF7Bn0I91o17GGkox0ZhhbE7.jpg"
+        }
+    ]
+},
+    {
     "title": "Moana موانا مدبلج",
     "year": "2016",
     "desc": "تدور أحداث الفيلم حول فتاة شجاعة تبحر في مغامرة خطيرة عبر المحيط لإنقاذ شعبها بمساعدة النصف إله ماوي لإعادة قلب الآلهة وتنسيق التوازن في الطبيعة",
