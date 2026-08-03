6b2d11be31039ef0ff9377a010f8806acadf2eff
Elena <mrmohmd5@gmail.com>
2026-08-03 14:22:59 +0300

    إضافة تلقائية ذكية ومباشرة للمادة: Into the Wild مع روابط الحلقات المرتبة

--- data.js
+++ data.js
@@ -1,5 +1,68 @@
 const globalMoviesData = [
     {
+    "title": "Into the Wild",
+    "year": "2007",
+    "desc": "بعد التخرج من جامعة إيموري في عام 1992 ، تخلى الطالب والرياضي كريستوفر ماكاندلس عن ممتلكاته ، وقدم كامل حساب التوفير البالغ 24000 دولار للجمعيات الخيرية ، وركوب الخيل في ألاسكا للعيش في البرية.",
+    "poster": "https://image.tmdb.org/t/p/w500/d09PojiAp7EXGFC4yJdfr4wBj3C.jpg",
+    "hdUrl": "https://streamtape.com/e/1JYo9g7JA6Uevkw/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "sdUrl": "https://streamtape.com/e/1JYo9g7JA6Uevkw/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "rating": "7.8",
+    "genre": "مغامرة دراما",
+    "type": "movie",
+    "cast": [
+        {
+            "name": "Emile Hirsch",
+            "character": "Christopher McCandless / Alexander Supertramp",
+            "image": "https://image.tmdb.org/t/p/w185/NQjX6KjtDDyLnvbxhrU9xQWaYJ.jpg"
+        },
+        {
+            "name": "Marcia Gay Harden",
+            "character": "Billie McCandless",
+            "image": "https://image.tmdb.org/t/p/w185/5vxcHIHABJxqhweAsTiXVOhT4SE.jpg"
+        },
+        {
+            "name": "William Hurt",
+            "character": "Walt McCandless",
+            "image": "https://image.tmdb.org/t/p/w185/j3mjmuHLBW4XQSw53C8Sh0Lh3ZQ.jpg"
+        },
+        {
+            "name": "Jena Malone",
+            "character": "Carine McCandless / Additional Narration (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/kGwV9iLmfUxJ6a73ehr0QvAnc8C.jpg"
+        },
+        {
+            "name": "Brian H. Dierker",
+            "character": "Rainey",
+            "image": "https://image.tmdb.org/t/p/w185/dcMKjuMB9XBJTnGUKoyAHOgI1iK.jpg"
+        },
+        {
+            "name": "Catherine Keener",
+            "character": "Jan Burres",
+            "image": "https://image.tmdb.org/t/p/w185/n4CTwGszs6cwS1wJRlDQ5Mlh7Ex.jpg"
+        },
+        {
+            "name": "Vince Vaughn",
+            "character": "Wayne Westerberg",
+            "image": "https://image.tmdb.org/t/p/w185/qImHvsd7SFJh46Ujx50xUfkeMNz.jpg"
+        },
+        {
+            "name": "Kristen Stewart",
+            "character": "Tracy Tatro",
+            "image": "https://image.tmdb.org/t/p/w185/nThfzR0nBamTyv2OIMz9udSXXB2.jpg"
+        },
+        {
+            "name": "Hal Holbrook",
+            "character": "Ronald \"Ron\" Franz",
+            "image": "https://image.tmdb.org/t/p/w185/rjUwo6mSutAljAHTqGhlnBBd9lO.jpg"
+        },
+        {
+            "name": "Thure Lindhardt",
+            "character": "Mads",
+            "image": "https://image.tmdb.org/t/p/w185/lDkRe6dlQoiehjay8pEP2WLU4qG.jpg"
+        }
+    ]
+},
+    {
     "title": "Moana 2",
     "year": "2024",
     "desc": "بعد تلقيها نداء من أسلافها، تسافر 'موانا' و'ماوي' إلى بحار 'أوقيانوسيا' البعيدة.",
