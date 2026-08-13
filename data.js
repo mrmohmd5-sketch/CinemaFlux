21872eac5bece212491368a39ebd2131d4e5cffa
Elena <mrmohmd5@gmail.com>
2026-08-13 22:57:22 +0300

    إضافة تلقائية للمادة: Elize: Shadows of a Woman

--- data.js
+++ data.js
@@ -1,5 +1,69 @@
 const globalMoviesData = [
     {
+    "title": "Elize: Shadows of a Woman",
+    "year": "2026",
+    "desc": "في هذه الرواية لقصة حقيقية، تكتشف فتاة كانت تعمل مرافقة خيانة زوجها الثري وتكافح للتأقلم مع الوضع بينما تسمم الخيانة زواجهما.",
+    "poster": "https://image.tmdb.org/t/p/w500/612ZyfilK8CVEEFphgPb3GOw0NF.jpg",
+    "hdUrl": "https://streamtape.com/e/JJ6pK9Pbm8fZbD/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "sdUrl": "https://streamtape.com/e/JJ6pK9Pbm8fZbD/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "rating": "7.4",
+    "genre": "إثارة جريمة دراما",
+    "type": "movie",
+    "cast": [
+        {
+            "name": "Lorena Comparato",
+            "character": "Elize Matsunaga",
+            "image": "https://image.tmdb.org/t/p/w185/cWF3OGztHzaHNiGQU4C14RV5G95.jpg"
+        },
+        {
+            "name": "Henrique Kimura",
+            "character": "Marcos Matsunaga",
+            "image": "https://image.tmdb.org/t/p/w185/kTlKFf27pfVKAyug8VxOnLue1Of.jpg"
+        },
+        {
+            "name": "Denise Weinberg",
+            "character": "Tia Marta",
+            "image": "https://image.tmdb.org/t/p/w185/l4XZHjhc7GFjTfxBp3mqlHrvz6N.jpg"
+        },
+        {
+            "name": "Gustavo Falcão",
+            "character": "Heitor",
+            "image": "https://image.tmdb.org/t/p/w185/ghw4GN9zvCTS1HB2lCUJ3ekNWL1.jpg"
+        },
+        {
+            "name": "Julia Shimura",
+            "character": "Clarice",
+            "image": "https://image.tmdb.org/t/p/w185/vfK9XUlc5VK70jLnkJk2PTkp04x.jpg"
+        },
+        {
+            "name": "Flora Sandyá",
+            "character": "Babá",
+            "image": "https://image.tmdb.org/t/p/w185/liMmKjTuq3cPR82gAkaAbXw3wXC.jpg"
+        },
+        {
+            "name": "Edson Kameda",
+            "character": "Takashi",
+            "image": "https://image.tmdb.org/t/p/w185/wlJ8ISAwTM4sVbClbSs1Vu7sXzm.jpg"
+        },
+        {
+            "name": "Miwa Yanagizawa",
+            "character": "Masuyo",
+            "image": "https://image.tmdb.org/t/p/w185/fNtieBDuyibQOOp31OMzhe2qTvv.jpg"
+        },
+        {
+            "name": "Arthur Toyoshima",
+            "character": "Lauro",
+            "image": "https://via.placeholder.com/185x278?text=No+Image"
+        },
+        {
+            "name": "Julia Konrad",
+            "character": "Larissa \"aka\" Bianca",
+            "image": "https://image.tmdb.org/t/p/w185/4L9a0Ix7wwZ4l9R2D2qH97kcEcK.jpg"
+        }
+    ],
+    "trailerKey": "KiKnd3cusPo"
+},
+    {
     "title": "Love & Death",
     "year": "2023",
     "desc": "يتناول العمل السيرة الذاتية لزوجين دائمي التردد على الكنيسة تجمعهما حياة عائلية مميزة في بلدة صغيرة في تكساس، ولكن تأخذ الأمور منعطفًا غير متوقع حينما يلجأ أحدهما إلى العنف.",
