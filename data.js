3bc196bda471629afa7407374f62bcaf65bc2068
Elena <mrmohmd5@gmail.com>
2026-08-15 10:16:28 +0300

    إضافة تلقائية للمادة: Loop

--- data.js
+++ data.js
@@ -1,5 +1,39 @@
 const globalMoviesData = [
     {
+    "title": "Loop",
+    "year": "2020",
+    "desc": "فتاة مصابة بالتوحد، غير قادرة على الكلام، وصبي ثرثار، يتشاركان رحلة تجديف. لإكمال رحلتهما عبر بحيرة حضرية، يجب أن يتعلم كل منهما كيف يرى الآخر العالم.",
+    "poster": "https://image.tmdb.org/t/p/w500/ob7v5JIH7uEqEjATh80BGLOyfAB.jpg",
+    "hdUrl": "https://streamtape.com/e/mPmOgWGz4rSbYP7/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "sdUrl": "https://streamtape.com/e/mPmOgWGz4rSbYP7/\" width=\"800\" height=\"600\" allowfullscreen allowtransparency allow=\"autoplay\" scrolling=\"no\" frameborder=\"0\"></iframe>",
+    "rating": "6.7",
+    "genre": "رسوم متحركة عائلي دراما",
+    "type": "movie",
+    "cast": [
+        {
+            "name": "Madison Bandy",
+            "character": "Renee (voice)",
+            "image": "https://via.placeholder.com/185x278?text=No+Image"
+        },
+        {
+            "name": "Christiano \"Chachi\" Delgado",
+            "character": "Marcus (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/3YZYhf7ZJmPzbkT6Nzm1j2yut4C.jpg"
+        },
+        {
+            "name": "Louis Gonzales",
+            "character": "Camp Counselor (voice)",
+            "image": "https://image.tmdb.org/t/p/w185/58IsdAjs75hdOd3POOjUaJUOyvE.jpg"
+        },
+        {
+            "name": "Asher Brodkey",
+            "character": "Additional Voices (voice)",
+            "image": "https://via.placeholder.com/185x278?text=No+Image"
+        }
+    ],
+    "trailerKey": "J20kmqY28KI"
+},
+    {
     "title": "Elize: Shadows of a Woman",
     "year": "2026",
     "desc": "في هذه الرواية لقصة حقيقية، تكتشف فتاة كانت تعمل مرافقة خيانة زوجها الثري وتكافح للتأقلم مع الوضع بينما تسمم الخيانة زواجهما.",
